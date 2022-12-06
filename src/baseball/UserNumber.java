b366a263bf01c93ec6737fe7c8430f8986168eb0
Woogie95 <choisw950928@gmail.com>
2022-12-06 21:39:58 +0900

    refactor: 입력 숫자 검증 및 저장

--- src/baseball/UserNumber.java
+++ src/baseball/UserNumber.java
@@ -19,7 +19,7 @@ public class UserNumber {
                 (tempNumbers))));
     }
 
-    public List<String> divideUserNumber(String userNumber) { // 입력된 숫자 나누기
+    public List<String> divideUserNumber(String userNumber) {
         List<String> tempNumbers = new ArrayList<>();
         for (int i = 0; i < userNumber.length(); i++) {
             tempNumbers.add(String.valueOf(userNumber.charAt(i)));
@@ -27,14 +27,14 @@ public class UserNumber {
         return tempNumbers;
     }
 
-    public List<String> validateUserNumberLength(List<String> tempNumbers) { // 길이 확인
+    public List<String> validateUserNumberLength(List<String> tempNumbers) {
         if (tempNumbers.size() != 3) {
             throw new IllegalArgumentException("입력한 숫자가 3개가 아닙니다.");
         }
         return tempNumbers;
     }
 
-    public List<String> validateUserNumberOverlap(List<String> tempNumbers) { // 중복 확인
+    public List<String> validateUserNumberOverlap(List<String> tempNumbers) {
         Set<String> tempUserNumber = new LinkedHashSet<>(tempNumbers);
         if (tempUserNumber.size() != 3) {
             throw new IllegalArgumentException("입력한 숫자에 중복 숫자가 포함되어 있습니다.");
@@ -42,14 +42,14 @@ public class UserNumber {
         return tempNumbers;
     }
 
-    public List<String> validateUserNumberBlank(List<String> tempNumbers) { // 공백 확인
+    public List<String> validateUserNumberBlank(List<String> tempNumbers) {
         if (tempNumbers.contains(" ")) {
             throw new IllegalArgumentException("입력한 숫자에 공백이 포함되어 있습니다.");
         }
         return tempNumbers;
     }
 
-    public List<Integer> validateUserNumberType(List<String> tempNumbers) { // 타입 체크 하고 반환
+    public List<Integer> validateUserNumberType(List<String> tempNumbers) {
         List<Integer> userNumber = new ArrayList<>();
         for (String tempNumber : tempNumbers) {
             try {
