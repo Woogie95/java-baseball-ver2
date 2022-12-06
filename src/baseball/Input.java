80532f848ebb5e9dbec7d76c0108d49f1e4f2de8
Woogie95 <choisw950928@gmail.com>
2022-12-06 21:41:59 +0900

    feat: 입력 및 관련하여 출력 메시지

---
+++ src/baseball/Input.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return SCANNER.nextLine();
+    }
+
+    public static String inputRestartNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return SCANNER.nextLine();
+    }
+
+}
